bbe4e108d4683d3135af24928ff010bec0e1a442
abir <abir.belkaroui@gmail.com>
2024-07-09 12:10:04 +0200

    sidebar xx

--- servicefield.client/src/Components/sidebar/SideBar.jsx
+++ servicefield.client/src/Components/sidebar/SideBar.jsx
@@ -1,123 +1,9 @@
 
-//import './SideBar.css';
-//const Sidebar = () => {
-//    return (
-//        <div id="mySidebar" className="sidebar">
-//            <a href="#">
-//                <span><i className="material-icons">info</i><span className="icon-text">about</span></span>
-//            </a><br />
-//            <a href="#">
-//                <i className="material-icons">spa</i><span className="icon-text">services</span>
-//            </a><br />
-//            <a href="#">
-//                <i className="material-icons">monetization_on</i><span className="icon-text">clients</span>
-//            </a><br />
-//            <a href="#">
-//                <i className="material-icons">email</i><span className="icon-text">contact</span>
-//            </a>
-//        </div>
-//    );
-//}
-
-//export default Sidebar;
-
-//import './SideBar.css';
-
-//import { Link } from 'react-router-dom';
-
-//const testIn = () => {
-//    console.log("hovering in sidebar");
-//}
-
-//const testOut = () => {
-//    console.log("hovering outside sidebar");
-//}
-
-//const Sidebar = () => {
-//    return (
-//        <div id="mySidebar" className="sidebar" onMouseOver={testIn} onMouseOut={testOut}>
-//            <Link to="/DetailsFormCase" className="sidebar-link">
-//                <i className="material-icons">description</i><span className="icon-text">Details Form Case</span>
-//            </Link>
-
-
-//            <a href="#">
-//                <i className="material-icons">home</i><span className="icon-text">home</span>
-//            </a>
-//            <a href="#">
-//                <i className="material-icons">spa</i><span className="icon-text">services</span>
-//            </a>
-//            <a href="#">
-//                <i className="material-icons">monetization_on</i><span className="icon-text">clients</span>
-//            </a>
-//            <a href="#">
-//                <i className="material-icons">email</i><span className="icon-text">contact</span>
-//            </a>
-//        </div>
-//    );
-//}
-
-
-//export default Sidebar;
-
-
-
-//////code3
-
-//import './SideBar.css';
-//import { useState } from 'react';
-//import { Link } from 'react-router-dom';
-
-//const Sidebar = () => {
-//    const [mini, setMini] = useState(true);
-
-//    const toggleSidebar = () => {
-//        if (mini) {
-//            console.log("opening sidebar");
-//            document.getElementById("mySidebar").style.width = "250px";
-//           /* document.getElementById("main").style.marginLeft = "250px";*/
-//            setMini(false);
-//        } else {
-//            console.log("closing sidebar");
-//            document.getElementById("mySidebar").style.width = "85px";
-//          /*  document.getElementById("main").style.marginLeft = "85px";*/
-//            setMini(true);
-//        }
-//    };
-
-//    return (
-//        <div>
-//            <div id="mySidebar" className="sidebar" onMouseOver={toggleSidebar} onMouseOut={toggleSidebar}>
-//                <a href="#">
-//                    <i className="material-icons">home</i><span className="icon-text">Home</span>
-//                </a><br />
-//                <Link to="/DetailsFormCase" className="sidebar-link">
-//                               <i className="material-icons">description</i><span className="icon-text">Details</span>
-//                </Link>
-              
-
-//                <a href="#">
-//                    <i className="material-icons">spa</i><span className="icon-text">services</span>
-//                </a><br />
-//                <a href="#">
-//                    <i className="material-icons">monetization_on</i><span className="icon-text">clients</span>
-//                </a><br />
-//                <a href="#">
-//                    <i className="material-icons">email</i><span className="icon-text">contact</span>
-//                </a>
-//            </div>
-//            <div id="main">
-                
-//            </div>
-//        </div>
-//    );
-//};
-
-//export default Sidebar;
 
 import './SideBar.css';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+//import img from "../../assets/mobile.png";
 
 const Sidebar = () => {
     const [mini, setMini] = useState(true);
@@ -162,6 +48,11 @@ const Sidebar = () => {
                 onMouseOver={handleMouseOver}
                 onMouseOut={handleMouseOut}
             >
+                {/*<img src={img} / > */}
+            <Link to="/Sidebar" className="sidebar-link">
+                    <i className="material-icons">home</i>
+                    <span className="icon-text">Home</span>
+                </Link><br />
                 <Link to="/DetailsFormCase" className="sidebar-link">
                     <i className="material-icons">description</i>
                     <span className="icon-text">Details</span>
