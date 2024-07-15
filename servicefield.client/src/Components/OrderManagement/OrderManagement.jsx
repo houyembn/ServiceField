3fb1241e849aa2aba588e7d13eee3d6515fb8663
houyem ben arbia <benarbia.houyem62@gmail.com>
2024-07-15 16:10:15 +0100

    form

--- servicefield.client/src/Components/OrderManagement/OrderManagement.jsx
+++ servicefield.client/src/Components/OrderManagement/OrderManagement.jsx
@@ -80,148 +80,186 @@ const OrderManagement = () => {
     };
 
     return (
-        <div className="order-management">
+       
             <div className="flex">
                 <SideBar />
                 <div className="flex-1">
-                    <ShowNavBar />
-            <div className="section">
-                        <h2>Service Order Form</h2>
-                        <div className='all'>
-                            <div className="form-fields">
-                                <CustomFormControl className="company-name" sx={{ minWidth: 120 }} size="small">
-                                    <InputLabel id="demo-select-small-label">Company Name</InputLabel>
-                                    <Select
-                                        labelId="demo-select-small-label"
-                                        id="demo-select-small"
-                                        value={CompanyName}
-                                        label="Company Name"
-                                        onChange={handleAgeChange}
-                                        sx={{ width: '300px' }} // Adjust width as needed
-                                    >
-                                        <MenuItem value={10}>L-mobile</MenuItem>
-                                        <MenuItem value={20}>houyem</MenuItem>
-                                    </Select>
-                                </CustomFormControl>
-
-                                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                    <DateTimePicker
-                                        label="Select Date and Time"
-                                        value={selectedDateTime}
-                                        onChange={handleDateTimeChange}
-                                        sx={{ width: '300px', marginLeft: '20px' }} // Adjust width and margin as needed
-                                        renderInput={(params) => (
-                                            <CustomTextField {...params} sx={{ width: '150px', m: 1 }} />
-                                        )}
-                                    />
-                                </LocalizationProvider>
-
-                                <CustomTextField
-                                    id="outlined-multiline-static"
-                                    label="Description"
-                                    multiline
-                                    rows={4}
-                                    sx={{ width: '300px', marginLeft: '20px' }} // Adjust width and margin as needed
-                                />
-                            </div>
-
-                            <div className='section2' style={{ display: 'flex', alignItems: 'flex-start', marginTop: '-40px', marginRight: '320px'  }}>
-                                <CustomFormControl className="servicetype" sx={{ minWidth: 120, marginRight: '20px' }} size="small">
-                                    <InputLabel id="demo-select-small-label">Service Type</InputLabel>
-                                    <Select
-                                        labelId="demo-select-small-label"
-                                        id="demo-select-small"
-                                        value={ServiceType}
-                                        label="Service Type"
-                                        sx={{ width: '300px'}} 
-                                        onChange={handleServiceTypeChange}
-                                    >
-                                        <MenuItem value={10}>Technical support</MenuItem>
-                                        <MenuItem value={20}>Maintenance</MenuItem>
-                                    </Select>
-                                </CustomFormControl>
-
-                                <CustomFormControl className="Assigned-To" sx={{ minWidth: 120 }} size="small">
-                                    <InputLabel id="demo-select-small-label">Assigned To</InputLabel>
-                                    <Select
-                                        labelId="demo-select-small-label"
-                                        id="demo-select-small"
-                                        value={AssignedTo}
-                                        label="Assigned To"
-                                        sx={{ width: '300px'}} 
-                                        onChange={handleAssignedToChange}
-                                    >
-                                        <MenuItem value={10}>Selim</MenuItem>
-                                        <MenuItem value={20}>Sara</MenuItem>
-                                    </Select>
-                                </CustomFormControl>
-                            </div>
-
-
-
-                            <div className='section3' style={{ display: 'flex', alignItems: 'center', marginTop: '30px' }}>
-                                <CustomFormControl className="Assigned-To" sx={{ minWidth: 120, marginRight: '20px', marginTop: '-80px' }} size="small">
-                                    <InputLabel id="demo-select-small-label">Status</InputLabel>
-                                    <Select
-                                        labelId="demo-select-small-label"
-                                        id="demo-select-small"
-                                        value={Status}
-                                        label="Status"
-                                        sx={{ width: '300px' }}
-                                        onChange={handleStatusChange}
-                                    >
-                                        {statusOptions.map(option => (
-                                            <MenuItem key={option.value} value={option.value}>{option.label}</MenuItem>
-                                        ))}
-                                    </Select>
-                                </CustomFormControl>
-
-                                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                    <div className='dateclosed' style={{ marginRight: '20px' ,marginTop: '-70px' }}>
-                                        <DateTimePicker
-                                            label="Select Date and Time"
-                                            value={selectedDateTimeClose}
-                                            onChange={handleDateTimeCloseChange}
-                                            sx={{ width: '300px' }} // Adjust width as needed
-                                            renderInput={(params) => (
-                                                <CustomTextField {...params} sx={{ width: '150px', m: 1 }} />
-                                            )}
-                                        />
-                                    </div>
-                                </LocalizationProvider>
-
-                                <div className="Resolution Summary">
-                                    <CustomTextField
-                                        id="outlined-multiline-static"
-                                        label="Resolution Summary"
-                                        multiline
-                                        rows={4}
-                                        sx={{ width: '300px' }} // Adjust width as needed
-                                    />
-                                </div>
-                            </div>
-
-                            <div className="CustomerSatisfaction" style={{ marginTop: '-55px', marginRight: '320px' }}>
-                                <CustomTextField
-                                    id="outlined-multiline-static"
-                                    label="Customer Satisfaction"
-                                    multiline
-                                    rows={1}
-                                    sx={{ width: '620px' }}
-                                />
-                            </div>
-
-                        </div>
+                <ShowNavBar />
+                <div className="p-4">
            
-                        <div className='button' style={{ display: 'flex', justifyContent: 'center', marginTop: '2px' }}>
-                            <Button variant="contained" type="submit">Submit</Button>
+                    <h2 className="h2">Service Order Form</h2>
+                    <div className='all'>
+                        <div className='row'>
+                            <TextField id="outlined-basic" className='input' label="Order Number" variant="outlined" size="small" />
+                            <TextField id="outlined-basic" className='input' label="Service Object" variant="outlined" size="small" />
                         </div>
+                        <div className='row'>
+                            <FormControl className='inputselect' size="small">
+                            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={CompanyName}
+                                label="Age"
+                                onChange={handleAgeChange}
+
+                            >
+                                <MenuItem value={10}>Ten</MenuItem>
+                                <MenuItem value={20}>Twenty</MenuItem>
+                                <MenuItem value={30}>Thirty</MenuItem>
+                            </Select>
+                        </FormControl>
+                            <FormControl className='inputselect'  size="small">
+                            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={CompanyName}
+                                label="Age"
+                                onChange={handleAgeChange}
+                            >
+                                <MenuItem value={10}>Ten</MenuItem>
+                                <MenuItem value={20}>Twenty</MenuItem>
+                                <MenuItem value={30}>Thirty</MenuItem>
+                            </Select>
+                        </FormControl>
+                        </div>
+                    </div>
+                    {/*    <div className='all'>*/}
+                    {/*        <div className="form-fields">*/}
+                    {/*            <CustomFormControl className="company-name" sx={{ minWidth: 120 }} size="small">*/}
+                    {/*                <InputLabel id="demo-select-small-label">Company Name</InputLabel>*/}
+                    {/*                <Select*/}
+                    {/*                    labelId="demo-select-small-label"*/}
+                    {/*                    id="demo-select-small"*/}
+                    {/*                    value={CompanyName}*/}
+                    {/*                    label="Company Name"*/}
+                    {/*                    onChange={handleAgeChange}*/}
+                    {/*                    sx={{ width: '300px' }} // Adjust width as needed*/}
+                    {/*                >*/}
+                    {/*                    <MenuItem value={10}>L-mobile</MenuItem>*/}
+                    {/*                    <MenuItem value={20}>houyem</MenuItem>*/}
+                    {/*                </Select>*/}
+                    {/*            </CustomFormControl>*/}
+
+                    {/*            <LocalizationProvider dateAdapter={AdapterDayjs}>*/}
+                    {/*                <DateTimePicker*/}
+                    {/*                    label="Select Date and Time"*/}
+                    {/*                    value={selectedDateTime}*/}
+                    {/*                    onChange={handleDateTimeChange}*/}
+                    {/*                    sx={{ width: '300px', marginLeft: '20px' }} // Adjust width and margin as needed*/}
+                    {/*                    renderInput={(params) => (*/}
+                    {/*                        <CustomTextField {...params} sx={{ width: '150px', m: 1 }} />*/}
+                    {/*                    )}*/}
+                    {/*                />*/}
+                    {/*            </LocalizationProvider>*/}
+
+                    {/*            <CustomTextField*/}
+                    {/*                id="outlined-multiline-static"*/}
+                    {/*                label="Description"*/}
+                    {/*                multiline*/}
+                    {/*                rows={4}*/}
+                    {/*                sx={{ width: '300px', marginLeft: '20px' }} // Adjust width and margin as needed*/}
+                    {/*            />*/}
+                    {/*        </div>*/}
+
+                    {/*        <div className='section2' style={{ display: 'flex', alignItems: 'flex-start', marginTop: '-40px', marginRight: '320px'  }}>*/}
+                    {/*            <CustomFormControl className="servicetype" sx={{ minWidth: 120, marginRight: '20px' }} size="small">*/}
+                    {/*                <InputLabel id="demo-select-small-label">Service Type</InputLabel>*/}
+                    {/*                <Select*/}
+                    {/*                    labelId="demo-select-small-label"*/}
+                    {/*                    id="demo-select-small"*/}
+                    {/*                    value={ServiceType}*/}
+                    {/*                    label="Service Type"*/}
+                    {/*                    sx={{ width: '300px'}} */}
+                    {/*                    onChange={handleServiceTypeChange}*/}
+                    {/*                >*/}
+                    {/*                    <MenuItem value={10}>Technical support</MenuItem>*/}
+                    {/*                    <MenuItem value={20}>Maintenance</MenuItem>*/}
+                    {/*                </Select>*/}
+                    {/*            </CustomFormControl>*/}
+
+                    {/*            <CustomFormControl className="Assigned-To" sx={{ minWidth: 120 }} size="small">*/}
+                    {/*                <InputLabel id="demo-select-small-label">Assigned To</InputLabel>*/}
+                    {/*                <Select*/}
+                    {/*                    labelId="demo-select-small-label"*/}
+                    {/*                    id="demo-select-small"*/}
+                    {/*                    value={AssignedTo}*/}
+                    {/*                    label="Assigned To"*/}
+                    {/*                    sx={{ width: '300px'}} */}
+                    {/*                    onChange={handleAssignedToChange}*/}
+                    {/*                >*/}
+                    {/*                    <MenuItem value={10}>Selim</MenuItem>*/}
+                    {/*                    <MenuItem value={20}>Sara</MenuItem>*/}
+                    {/*                </Select>*/}
+                    {/*            </CustomFormControl>*/}
+                    {/*        </div>*/}
+
+
+
+                    {/*        <div className='section3' style={{ display: 'flex', alignItems: 'center', marginTop: '30px' }}>*/}
+                    {/*            <CustomFormControl className="Assigned-To" sx={{ minWidth: 120, marginRight: '20px', marginTop: '-80px' }} size="small">*/}
+                    {/*                <InputLabel id="demo-select-small-label">Status</InputLabel>*/}
+                    {/*                <Select*/}
+                    {/*                    labelId="demo-select-small-label"*/}
+                    {/*                    id="demo-select-small"*/}
+                    {/*                    value={Status}*/}
+                    {/*                    label="Status"*/}
+                    {/*                    sx={{ width: '300px' }}*/}
+                    {/*                    onChange={handleStatusChange}*/}
+                    {/*                >*/}
+                    {/*                    {statusOptions.map(option => (*/}
+                    {/*                        <MenuItem key={option.value} value={option.value}>{option.label}</MenuItem>*/}
+                    {/*                    ))}*/}
+                    {/*                </Select>*/}
+                    {/*            </CustomFormControl>*/}
+
+                    {/*            <LocalizationProvider dateAdapter={AdapterDayjs}>*/}
+                    {/*                <div className='dateclosed' style={{ marginRight: '20px' ,marginTop: '-70px' }}>*/}
+                    {/*                    <DateTimePicker*/}
+                    {/*                        label="Select Date and Time"*/}
+                    {/*                        value={selectedDateTimeClose}*/}
+                    {/*                        onChange={handleDateTimeCloseChange}*/}
+                    {/*                        sx={{ width: '300px' }} // Adjust width as needed*/}
+                    {/*                        renderInput={(params) => (*/}
+                    {/*                            <CustomTextField {...params} sx={{ width: '150px', m: 1 }} />*/}
+                    {/*                        )}*/}
+                    {/*                    />*/}
+                    {/*                </div>*/}
+                    {/*            </LocalizationProvider>*/}
+
+                    {/*            <div className="Resolution Summary">*/}
+                    {/*                <CustomTextField*/}
+                    {/*                    id="outlined-multiline-static"*/}
+                    {/*                    label="Resolution Summary"*/}
+                    {/*                    multiline*/}
+                    {/*                    rows={4}*/}
+                    {/*                    sx={{ width: '300px' }} // Adjust width as needed*/}
+                    {/*                />*/}
+                    {/*            </div>*/}
+                    {/*        </div>*/}
+
+                    {/*        <div className="CustomerSatisfaction" style={{ marginTop: '-55px', marginRight: '320px' }}>*/}
+                    {/*            <CustomTextField*/}
+                    {/*                id="outlined-multiline-static"*/}
+                    {/*                label="Customer Satisfaction"*/}
+                    {/*                multiline*/}
+                    {/*                rows={1}*/}
+                    {/*                sx={{ width: '620px' }}*/}
+                    {/*            />*/}
+                    {/*        </div>*/}
+
+                    {/*    </div>*/}
+           
+                    {/*    <div className='button' style={{ display: 'flex', justifyContent: 'center', marginTop: '2px' }}>*/}
+                    {/*        <Button variant="contained" type="submit">Submit</Button>*/}
+                    {/*    </div>*/}
 
 
                     </div>
                 </div>
             </div>
-        </div>
+       
     );
 };
 
